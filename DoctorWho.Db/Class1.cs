de91e82d654f181430b0523cc4c85b503ef24b9d
nabeelkayed <nabeelkayed25@gmail.com>
2022-04-25 10:41:24 +0300

    Create DbContext

--- DoctorWho.Db/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace DoctorWho.Db
-{
-    public class Class1
-    {
-    }
-}
---
+++ DoctorWho.Db/DbContexts/DoctorWhoCoreDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DoctorWho.Db
+{
+    public class DoctorWhoCoreDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Data Source = DESKTOP-957A0UD\\SQLEXPRESS; User id = sa; password=nabeel; Initial Catalog = DoctorWhoCore;");
+        }
+    }
+}
